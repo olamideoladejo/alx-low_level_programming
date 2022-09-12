4ca3bb50dd74fd0ca2c787ace7e5324dd36ba44c
olamideoladejo <oladejoolamide07@gmail.com>
2022-09-12 11:03:05 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include	<stdio.h>
+/**
+ * main	-	Prints	the	alphabet	in	lowercase,	then	in	uppercase.
+ *
+ * Return:	0	on	sucess
+ */
+int	main(void)
+{
+	char	c	=	'a';
+	char	d	=	'A';
+
+	while	(c	<=	'z')
+			{
+				putchar(c);
+				c++;
+			}
+				while	(d	<=	'Z')
+			{
+				putchar(d);
+				d++;
+			}
+				putchar('\n');
+				return	(0);
+
+}
